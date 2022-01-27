748326ba1df897916462ef28ae01347ee1f678fc
Venera <venusik00@gmail.com>
2022-01-28 01:45:59 +0600

    commit project

---
+++ gitcommands.bat
@@ -0,0 +1,5 @@
+git add .
+git commit -m "commit project"
+git branch -M main
+git remote add origin https://github.com/kubashevav/placeholder.git
+git push -u origin main
---
+++ hello.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	password := "123"
+	println(password)
+}
